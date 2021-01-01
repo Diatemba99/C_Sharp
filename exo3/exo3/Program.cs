50caee010ec70854c6eb871426f5843d6ee195ae
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-01 23:59:14 +0000

    Add files via upload

---
+++ exo3/exo3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //DateTime aDate = DateTime.Now;
+            Console.WriteLine(DateTime.Now.ToString("dddd dd MMMM yyyy hh:mm:ss"));
+            Console.WriteLine(DateTime.Now.AddMinutes(15).ToString("dddd dd MMMM yyyy hh:mm:ss"));
+
+        }
+    }
+}
