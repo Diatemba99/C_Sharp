caa66ced20a55e41d83ee00269c289e5d617a116
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-01 23:58:26 +0000

    Add files via upload

---
+++ exo2/exo2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double  hauteur = 300, bounce = 0;
+            while (hauteur > 10)
+            {
+                bounce++;
+                hauteur *= 0.9 ;
+            }
+            Console.WriteLine("Il faudra de " + bounce + " rebond(s)!");
+        }
+    }
+}
