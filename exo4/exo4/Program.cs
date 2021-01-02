ca6cdbbac1b17139153a47be5865e508c544e564
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-02 00:00:13 +0000

    Add files via upload

---
+++ exo4/exo4/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace exo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            top:
+            int essai = 3;
+            Random valeur = new Random();
+            int nombreMystere = valeur.Next(100);
+
+
+            for ( int i = 0; i <3; i++ )
+            {
+          
+                Console.WriteLine("Saisir un nombre :");
+                int val = Convert.ToInt32(Console.ReadLine());
+
+                if(val == nombreMystere)
+                {
+                    Console.WriteLine("Bravo vous avez trouvé le nombre.");
+                    Console.WriteLine("Voulez vous rejouer [oui/non] ? :");
+                    string reponse = Convert.ToString(Console.ReadLine());
+                    if(reponse =="oui")
+                    {
+                        goto top;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+                else if (val > nombreMystere)
+                {
+                    essai--;
+                    Console.WriteLine(val + " est supérieur au nombre mystere");
+                    Console.WriteLine("Il vous reste " + essai +" tentative(s)");
+                    
+                    if(essai == 0)
+                    {
+                        Console.WriteLine("Vous avez perdu la manche");
+                        Console.WriteLine("Voulez vous rejouer [oui/non] ? :");
+                        string reponse = Convert.ToString(Console.ReadLine());
+                        if (reponse == "oui")
+                        {
+                            goto top;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                else if (val < nombreMystere)
+                {
+                    essai--;
+                    Console.WriteLine(val + " est inférieur au nombre mystere");
+                    Console.WriteLine("Il vous reste " + essai + " tentative(s)");
+
+                    if (essai == 0)
+                    {
+                        Console.WriteLine("Vous avez perdu la manche");
+                        Console.WriteLine("Voulez vous rejouer [oui/non] ? :");
+                        string reponse = Convert.ToString(Console.ReadLine());
+                        if (reponse == "oui")
+                        {
+                            goto top;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                }
+            }
+           
+            
+
+        }
+    }
+}
