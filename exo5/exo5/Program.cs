1c7f3938aeeda064d6108de726c99653728b1566
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-02 00:48:12 +0000

    Add files via upload

---
+++ exo5/exo5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            top:;
+            long number = 1;
+            do
+            {
+                Console.WriteLine("Saisissez le nombre: ");
+                number = Convert.ToInt32(Console.ReadLine());
+            } while (number > 20);
+           
+
+            long result = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine("Le factoriel est : " + result);
+            Console.WriteLine("Voulez-vous effectuer un nouveau calcul ? [oui/non] ");
+            string reponse = Convert.ToString(Console.ReadLine());
+            
+            if(reponse == "oui")
+            {
+                goto top;
+            }
+           
+        }
+    }
+}
