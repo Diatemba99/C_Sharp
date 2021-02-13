f75b7e9f264bcd272c2677c391c54cd961c2ef69
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-02-14 00:33:26 +0100

    Update Program.cs

--- exo3/exo3/Program.cs
+++ exo3/exo3/Program.cs
@@ -10,7 +10,6 @@ namespace exo3
             //DateTime aDate = DateTime.Now;
             Console.WriteLine(DateTime.Now.ToString("dddd dd MMMM yyyy hh:mm:ss"));
             Console.WriteLine(DateTime.Now.AddMinutes(15).ToString("dddd dd MMMM yyyy hh:mm:ss"));
-
         }
     }
 }
